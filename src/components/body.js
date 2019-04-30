73682d35a2e91f1346ec050fdf620129aeb05bc3
Susanne Gut <Susanne@Antons-MacBook-Pro.local>
2019-04-30 11:28:45 +0200

    fifth commit

--- src/components/body.js
+++ src/components/body.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./layout.css"
 import {StaticQuery, graphql} from "gatsby"
-import GatsbyImage from "gatsby-image"
+import Img from "gatsby-image"
 
 const Body = () => (
   <StaticQuery
@@ -16,6 +16,7 @@ const Body = () => (
                 text
             }
             ... on DatoCmsImage{
+                model {apiKey}
                 img{
                     fluid(maxWidth: 500, imgixParams: {fm: "jpg", auto:"compress" }) {
                         ...GatsbyDatoCmsFluid
@@ -29,14 +30,13 @@ const Body = () => (
       }
     `}
     render = {data => {
-
       return (
             <div>
              {data.datoCmsPage.content.map((block) => (
-               <div>
-                   <h4>{block.h4}</h4>
-                   <div>{block.text}</div>
-                    <GatsbyImage fluid={block.img}/>
+               <div style={{border: "1px solid black"}}>
+               {block.h4 ? <h4>{block.h4}</h4> : ''}
+               {block.text ? <p>{block.text}</p> : ''}
+               {block.img ? <div style={{maxWidth: `100px`, marginBottom: `1.45rem`}}><Img fluid={block.img.fluid}/> </div>: ''}
                 </div>
                ))
               }
